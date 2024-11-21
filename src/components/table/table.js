51eef48d8a22730d29c1d8fb0c3b2bfe1a2b4dd3
joaquin Batemarco <joacobatemarco@gmail.com>
2024-11-21 14:56:58 -0300

    tablas scrolleables

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -12,54 +12,55 @@ function StarRating({ rating }) {
 }
 
 function Table({ data, columns, onRowClick }) {
-
     return (
-        <table className="table">
-            <thead className="table-head">
-                <tr className="column-names">
-                    {columns.map((col, index) => (
-                        <th key={index}>{col.name}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody className="table-body">
-                {data.map((row, index) => (
-                    <tr 
-                        className="table-card" 
-                        key={index} 
-                        onClick={() => onRowClick(row)}
-                    >
-                        {columns.map((col, i) => (
-                            <td key={i}>
-                                {col.isImage ? (
-                                    row[col.name.toLowerCase()] ? (
-                                        <div className="table-card-logo">
-                                            <img 
-                                                src={row[col.name.toLowerCase()]} 
-                                                alt={col.name} 
-                                            />
-                                        </div>
-                                    ) : (
-                                        <div className="table-card-logo">
-                                            <img 
-                                                src="path/to/default-image.png"
-                                                alt="default" 
-                                            />
-                                        </div>
-                                    )
-                                ) : col.name === 'Puntuación' ? (
-                                    <StarRating rating={row.puntuacion} />
-                                ) : (
-                                    <div className="table-card-subcontainer">
-                                        {row[col.name.toLowerCase()]}
-                                    </div>
-                                )}
-                            </td>
+        <div className="table-container">
+            <table className="table">
+                <thead className="table-head">
+                    <tr className="column-names">
+                        {columns.map((col, index) => (
+                            <th key={index}>{col.name}</th>
                         ))}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="table-body">
+                    {data.map((row, index) => (
+                        <tr 
+                            className="table-card" 
+                            key={index} 
+                            onClick={() => onRowClick(row)}
+                        >
+                            {columns.map((col, i) => (
+                                <td key={i}>
+                                    {col.isImage ? (
+                                        row[col.name.toLowerCase()] ? (
+                                            <div className="table-card-logo">
+                                                <img 
+                                                    src={row[col.name.toLowerCase()]} 
+                                                    alt={col.name} 
+                                                />
+                                            </div>
+                                        ) : (
+                                            <div className="table-card-logo">
+                                                <img 
+                                                    src="/logo192.png"
+                                                    alt="default" 
+                                                />
+                                            </div>
+                                        )
+                                    ) : col.name === 'Puntuación' ? (
+                                        <StarRating rating={row.puntuacion} />
+                                    ) : (
+                                        <div className="table-card-subcontainer">
+                                            {row[col.name.toLowerCase()]}
+                                        </div>
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
